8a1e546de12f7bfb48dff5745275447ef33a99e5
Vasanth-V1 <vasanth.v2023@vitstudent.ac.in>
2026-01-08 20:18:37 +0530

    Updating vasanth1 and adding vasanth2

--- vasanth1.py
+++ vasanth1.py
@@ -6,9 +6,4 @@ sum = num1 + num2
 
 # Display the sum using an f-string
 print(f"The sum of {num1} and {num2} is: {sum}")
-<<<<<<< HEAD
-print(num1)
-#Terminal Stash
-=======
-print(num1)
->>>>>>> 6aaa6c3b415bd232d203342626fe88b2f56bb5af
+
